e200c8ae03499d8ab3c41afcb18cc9c23085a77d
azab9 <azab91@gmail.com>
2022-07-15 16:42:50 +0200

    feat(DarklandPlayer): random character name for fun

--- Assets/_Darkland/Sources/Scripts/DarklandPlayer.cs
+++ Assets/_Darkland/Sources/Scripts/DarklandPlayer.cs
@@ -1,14 +1,27 @@
 using System;
+using _Darkland.Sources.Models;
 using Mirror;
 
 namespace _Darkland.Sources.Scripts {
 
     public class DarklandPlayer : NetworkBehaviour {
 
+        //todo tmp
+        [field: SyncVar]
+        public string characterName { get; private set; }
+
         public event Action ClientStarted;
         public static event Action LocalPlayerStarted;
         public static event Action LocalPlayerStopped;
         public static DarklandPlayer localPlayer;
+
+        public override void OnStartServer() {
+            characterName = $"{CharacterNames.RandomName()} {CharacterNames.RandomName()}";
+        }
+
+        public override void OnStartClient() {
+            ClientStarted?.Invoke();
+        }
 
         public override void OnStartLocalPlayer() {
             localPlayer = this;
@@ -17,10 +30,6 @@ namespace _Darkland.Sources.Scripts {
 
         public override void OnStopLocalPlayer() {
             LocalPlayerStopped?.Invoke();
-        }
-
-        public override void OnStartClient() {
-            ClientStarted?.Invoke();
         }
     }
 
