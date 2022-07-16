3aed9d1c707a38963bd79878add6cb3674ef0799
azab9 <azab91@gmail.com>
2022-07-16 12:00:35 +0200

    feat(Presentation): BasicClientInteractionsPanel

--- Assets/_Darkland/Sources/Scripts/DarklandPlayer.cs
+++ Assets/_Darkland/Sources/Scripts/DarklandPlayer.cs
@@ -1,6 +1,7 @@
 using System;
 using _Darkland.Sources.Models;
 using Mirror;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace _Darkland.Sources.Scripts {
---
+++ Assets/_Darkland/Sources/Scripts/Presentation/BasicClientInteractionsPanel.cs
@@ -0,0 +1,55 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Darkland.Sources.Scripts.Presentation {
+
+    public class BasicClientInteractionsPanel : MonoBehaviour {
+
+        [SerializeField]
+        private Button startClientButton;
+        
+        [SerializeField]
+        private Button stopClientButton;
+
+        [SerializeField]
+        private Button quitGameButton;
+
+        private void Start() {
+            stopClientButton.interactable = false;
+        }
+
+        private void OnEnable() {
+            startClientButton.onClick.AddListener(StartClient);
+            stopClientButton.onClick.AddListener(StopClient);
+            quitGameButton.onClick.AddListener(QuitGame);
+        }
+
+        private void OnDisable() {
+            startClientButton.onClick.RemoveListener(StartClient);
+            stopClientButton.onClick.RemoveListener(StopClient);
+            quitGameButton.onClick.RemoveListener(QuitGame);
+        }
+
+        private void StartClient() {
+            NetworkManager.singleton.networkAddress = "70.34.242.30"; //todo hehe
+            NetworkManager.singleton.StartClient();
+
+            startClientButton.interactable = false;
+            stopClientButton.interactable = true;
+        }
+
+        private void StopClient() {
+            NetworkManager.singleton.StopClient();
+            
+            startClientButton.interactable = true;
+            stopClientButton.interactable = false;
+        }
+
+        private void QuitGame() {
+            StopClient();
+            Application.Quit();
+        }
+    }
+
+}
