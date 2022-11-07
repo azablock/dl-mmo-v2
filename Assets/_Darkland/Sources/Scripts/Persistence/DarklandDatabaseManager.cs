c2a2e8cbdefb9a834a26c3a0daeb70a243eb81fb
azab9 <azab91@gmail.com>
2022-11-07 12:05:49 +0100

    feat(MongoDB): drivers (.dll) setup

---
+++ Assets/_Darkland/Sources/Scripts/Persistence/DarklandDatabaseManager.cs
@@ -0,0 +1,26 @@
+using MongoDB.Driver;
+using UnityEngine;
+
+namespace _Darkland.Sources.Scripts.Persistence {
+
+    public class DarklandDatabaseManager : MonoBehaviour {
+
+        private IMongoDatabase _db;
+        private MongoClient _client;
+
+        private void Awake() {
+            const string hostname = "70.34.242.30";
+            const string dbName = "darkland";
+            var uri = $"mongodb://root:rootpassword@{hostname}:27017";
+
+            _client = new MongoClient(uri);
+            _db = _client.GetDatabase(dbName);
+
+            Debug.LogWarning("================ DB connected ================");
+
+            _db.ListCollectionNames()
+               .ForEachAsync((s, i) => { Debug.LogWarning($"collection[{i}] name: {s}"); });
+        }
+    }
+
+}
