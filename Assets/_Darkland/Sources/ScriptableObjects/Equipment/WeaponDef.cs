06bc814bfc0f01b787e834dd63bc873fc1c09423
azab9 <azab91@gmail.com>
2023-02-12 15:58:12 +0100

    unityeditor dependency v2

--- Assets/_Darkland/Sources/ScriptableObjects/Equipment/WeaponDef.cs
+++ Assets/_Darkland/Sources/ScriptableObjects/Equipment/WeaponDef.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using _Darkland.Sources.Models.Equipment;
-using UnityEditor;
 using UnityEngine;
 
 namespace _Darkland.Sources.ScriptableObjects.Equipment {
@@ -60,39 +59,6 @@ namespace _Darkland.Sources.ScriptableObjects.Equipment {
                    $"{bonuses}";
         }
 
-    }
-    
-    [CustomEditor(typeof(WeaponDef))]
-//We need to extend the Editor
-    public class WeaponEditor : Editor
-    {
-        //Here we grab a reference to our Weapon SO
-        WeaponDef weapon;
-
-        private void OnEnable()
-        {
-            //target is by default available for you
-            //because we inherite Editor
-            weapon = target as WeaponDef;
-        }
-
-        //Here is the meat of the script
-        public override void OnInspectorGUI()
-        {
-            //Draw whatever we already have in SO definition
-            base.OnInspectorGUI();
-            //Guard clause
-            if (weapon.Sprite == null)
-                return;
-
-            //Convert the weaponSprite (see SO script) to Texture
-            Texture2D texture = AssetPreview.GetAssetPreview(weapon.Sprite);
-            //We crate empty space 80x80 (you may need to tweak it to scale better your sprite
-            //This allows us to place the image JUST UNDER our default inspector
-            GUILayout.Label("", GUILayout.Height(80), GUILayout.Width(80));
-            //Draws the texture where we have defined our Label (empty space)
-            GUI.DrawTexture(GUILayoutUtility.GetLastRect(), texture);
-        }
     }
 
 }
--- Assets/_Darkland/Sources/ScriptableObjects/Mob/MobDef.cs
+++ Assets/_Darkland/Sources/ScriptableObjects/Mob/MobDef.cs
@@ -1,7 +1,6 @@
 using System.Text.RegularExpressions;
 using _Darkland.Sources.Models.Combat;
 using _Darkland.Sources.Scripts.Interaction;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -65,38 +64,5 @@ namespace _Darkland.Sources.ScriptableObjects.Mob {
         public int RespawnTime => respawnTime;
 
     }
-    
-    [CustomEditor(typeof(MobDef))]
-    public class MobDefEditor : Editor
-    {
-        //Here we grab a reference to our Weapon SO
-        public MobDef mob;
-
-        private void OnEnable()
-        {
-            //target is by default available for you
-            //because we inherite Editor
-            mob = target as MobDef;
-        }
-
-        //Here is the meat of the script
-        public override void OnInspectorGUI()
-        {
-            //Draw whatever we already have in SO definition
-            base.OnInspectorGUI();
-            //Guard clause
-            if (mob.MobSprite == null)
-                return;
-
-            //Convert the weaponSprite (see SO script) to Texture
-            Texture2D texture = AssetPreview.GetAssetPreview(mob.MobSprite);
-            //We crate empty space 80x80 (you may need to tweak it to scale better your sprite
-            //This allows us to place the image JUST UNDER our default inspector
-            GUILayout.Label("", GUILayout.Height(80), GUILayout.Width(80));
-            //Draws the texture where we have defined our Label (empty space)
-            GUI.DrawTexture(GUILayoutUtility.GetLastRect(), texture);
-        }
-    }
-
 
 }
